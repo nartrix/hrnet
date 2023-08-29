c45dce1cc9be66334498b3f8c5615314192836d1
nartrix <dimitri.danielo@laposte.net>
2023-08-29 19:18:54 +0200

    add npm package datatable

--- src/pages/employee-list/index.jsx
+++ src/pages/employee-list/index.jsx
@@ -1,5 +1,12 @@
+import { Table } from "d-danielo-data-table-oc/dist/";
+
 function employeeList() {
-  return <></>;
+  const dataEmployees = JSON.parse(localStorage.getItem("EmployeesList")) || [];
+  return (
+    <>
+      <Table data={dataEmployees} />
+    </>
+  );
 }
 
 export default employeeList;
